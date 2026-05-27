53c2421af3ed84761b4a031c53fd07598d01590e
Dhanu Pratap <dhanupratap15@gmail.com>
2026-05-27 20:47:25 +0530

    commit 2

--- AI PDF ANSWERMATE/Pdf.py
+++ AI PDF ANSWERMATE/Pdf.py
@@ -1,7 +1,14 @@
 #import
+from mistralai import Mistral
+from dotenv import load_dotenv
+import os 
+load_dotenv()
+client = Mistral(
+    api_key=os.getenv("MISTRAL_API_KEY")
+    )
 from PyPDF2 import PdfReader
 #load pdf
-reader = PdfReader("sample.pdf")
+reader = PdfReader("sample1.pdf")
 # show text
 for i , page in enumerate(reader.pages):
     text=page.extract_text()
@@ -18,9 +25,9 @@ for line in lines :
             questions.append(q)
 
 # Use AI
-import requests
-url = "http://localhost:11434/api/generate"
-print("Making....")
+# import requests
+# url = "http://localhost:11434/api/generate"
+# print("Making....")
 for i,pro in enumerate(questions):
     prompt = f"""
     Question: {pro}
@@ -28,15 +35,29 @@ for i,pro in enumerate(questions):
     examples, and conclusion  using
     English style for easy understanding.
     """
-    print(f"{i}. {pro}")
-    response = requests.post(
-        url,
-        json={
-            "model": "llama3.2",
-            "prompt": prompt,
-            "stream": False
-        }
-    )
-    data = response.json()['response']
+    print(f"{i+1}. {pro}")
+    # response = requests.post(
+    #     url,
+    #     json={
+    #         "model": "llama3.2",
+    #         "prompt": prompt,
+    #         "stream": False
+    #     }
+    # )
+    # data = response.json()['response']
 
-    print(data)    
+    # print(data)    
+    ####USE MISTRAL API 
+   
+    response = client.chat.complete(
+        model="ministral-3b-latest",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+    opt= response.choices[0].message.content
+    with open("answers.txt", "w",encoding="utf-8") as file:
+        file.write(pro + "\n" + opt)  
